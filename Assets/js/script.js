4a7e44ea9bdad506eb3c5754a0c2f1d48891058d
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-06 15:28:41 -0600

    added initial getRandom functions for uppercase, lowercase, symbols, and numeric values

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+function getRandomLowercase() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz".split;
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
 
+function getRandomUppercase() {
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split;
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function getRandomSymbol() {
+  var symbols = "!@#$%&*?".split;
+  return symbols[Math.floor(Math.random() * symbol.length)];
+}
+
+function getRandomNumber() {
+  var numbers = "0123456789".split;
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
