38c74d767909e89caf5950755fd29396b251b12e
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-05-07 18:44:29 -0600

    added functionality to user choice of password length...definitley needs some tweaking

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,21 +16,18 @@ function generatePassword() {
     getRandomSymbol();
   } else if (addNumber === true) {
     getRandomNumber();
-  } else if (passwordLength) {
-  //   lengthSelection();
-  }
-  return(randomLowerCase + randomUpperCase + randomSymbol + randomNumber);
+  } 
+  return(passwordLength+ randomLowerCase + randomUpperCase + randomSymbol + randomNumber * passwordLength);
 }
+
 
 function lengthSelection() {
-  var fieldLength = (128 >= 8);
-  return (password + fieldLength);
+  var userLength = prompt("Please choose a password length between 8 - 128 characters."); 
+  var length = parseInt(8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60);
+  for (var i = 8; i <= length; i++);
+  return length + userLength;
 }
-
-
-
-
-
+console.log(typeof passwordLength);
 
 
 
@@ -63,6 +60,8 @@ if (addNumber === true) {
   null;
 
 
+
+  // "getRandom" Functions
 function getRandomLowercase() {
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   return lowerCase[Math.floor(Math.random() * lowerCase.length)];
@@ -83,6 +82,8 @@ function getRandomNumber() {
   return numbers[Math.floor(Math.random() * numbers.length)];
 } console.log(getRandomNumber);
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -90,11 +91,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword);
